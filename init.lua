f2cf7f08a21c0b6be9c073fa5ea169088f236cfe
antonio-pas <antonio.b.passos18@gmail.com>
2025-12-11 06:15:53 +0000

    Initial commit

---
+++ init.lua
@@ -0,0 +1,59 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartindent = true
+
+vim.opt.winborder = "none"
+vim.opt.signcolumn = 'yes:1'
+
+vim.opt.hidden = true
+vim.opt.scrolloff = 4
+vim.opt.wrap = false
+vim.opt.hlsearch = false
+vim.opt.showmode = false
+vim.opt.termguicolors = true
+vim.diagnostic.config({ virtual_text = true })
+vim.cmd("set completeopt+=noselect,fuzzy")
+vim.opt.background = "dark"
+vim.cmd.colorscheme "retrobox"
+vim.g.mapleader = " "
+vim.opt.mouse = 'a'
+
+vim.pack.add({
+  'https://github.com/nvim-mini/mini.nvim',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/folke/lazydev.nvim'
+})
+
+require('lazydev').setup()
+
+require('mini.jump2d').setup({})
+require('mini.git').setup({})
+require('mini.files').setup({})
+require('mini.clue').setup({})
+require('mini.pick').setup({})
+require('mini.move').setup({})
+require('mini.pairs').setup({})
+require('mini.surround').setup({})
+require('mini.tabline').setup({})
+require('mini.notify').setup({})
+require('mini.statusline').setup({})
+require('mini.trailspace').setup({})
+require('mini.completion').setup({})
+require('mini.icons').setup({})
+require('mini.snippets').setup({})
+
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('ts_ls')
+
+local pick = require('mini.pick')
+
+vim.keymap.set('n', '<leader>ff', pick.builtin.files)
+vim.keymap.set('n', '<leader>lg', pick.builtin.grep_live)
